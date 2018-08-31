a1ed3251e07a6e1053b48afd3b60f8732db37de4
abshaynes <42352506+abshaynes@users.noreply.github.com>
2018-08-30 21:05:11 -0500

    Delete plot1.R

--- plot1.R
+++
@@ -1,5 +0,0 @@
-## To Create Plot 1 Histogram
-hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
-## To Save Plot 1
-dev.copy(png, "plot1.png", width = 480, height = 480)
-dev.off()
